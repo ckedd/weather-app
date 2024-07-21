eaa32edde0046da7f1d9657ecc69376c0150b09d
chris-k-njit <ck378@njit.edu>
2024-07-21 18:09:07 -0400

    Git ignore added, adjusted node modules.

---
+++ src/db.ts
@@ -0,0 +1,12 @@
+import { MongoClient } from 'mongodb';
+
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+async function connectDB() {
+  await client.connect();
+  console.log('Connected to MongoDB');
+  return client.db('weatherApp');
+}
+
+export default connectDB;
